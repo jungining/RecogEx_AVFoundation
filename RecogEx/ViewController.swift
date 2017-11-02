3bfbcaa1dd01f7edc2927e8c569ecc033711ef60
YuJungin <jungin641@gmail.com>
2017-11-02 20:47:47 +0900

    init proj

---
+++ RecogEx/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  RecogEx
+//
+//  Created by YuJungin on 2017. 11. 2..
+//  Copyright © 2017년 junginin. All rights reserved.
+//
+
+import UIKit
+import AVKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
