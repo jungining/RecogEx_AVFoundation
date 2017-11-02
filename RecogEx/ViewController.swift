eed3ff53b7cffb7feb700e8d9c8407095a9f5412
YuJungin <jungin641@gmail.com>
2017-11-02 20:54:08 +0900

    get Camera views from back camera

--- RecogEx/ViewController.swift
+++ RecogEx/ViewController.swift
@@ -14,6 +14,15 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let captureSession = AVCaptureSession()
+        
+        guard let captureDevice = AVCaptureDevice.default(for: .video) //프리뷰로 볼거니까 비디오 타입
+            else { return }
+        
+        guard let input = try? AVCaptureDeviceInput(device : captureDevice) else { return }
+        
+        captureSession.addInput(input)
+        
         
     }
 
